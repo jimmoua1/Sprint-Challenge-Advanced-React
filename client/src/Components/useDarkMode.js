63709a6d446dc7e4ebb5f4e048750e60be7795c8
jimmoua1 <tsheejmaus@yahoo.com>
2020-04-17 16:41:48 -0700

    finilized Darmode

--- client/src/Components/useDarkMode.js
+++ client/src/Components/useDarkMode.js
@@ -5,6 +5,13 @@ import useLocalStorage from './localStorage'
 const useDarkMode = () => {
     const [values, setValues] = useLocalStorage("darkMode");
     useEffect(()=> {
+        if (values === true) {
+            document.body.classList.add("dark-mode")
+        } else {
+            document.body.classList.remove("dark-mode")
+        }
+    },[values])
+    return [values , setValues]
 }
 
-export default useDarkMode
+export default useDarkMode 
