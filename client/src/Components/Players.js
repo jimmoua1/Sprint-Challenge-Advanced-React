80b1e12bf9536c66ca8ce9a44bc4b438b0bef2b1
jimmoua1 <54656947+jimmoua1@users.noreply.github.com>
2020-04-17 17:36:24 -0700

    Jim moua (#2)
    * added components files
    
    * starting components js files
    
    * useDarkMode.js
    
    * finilized Darmode
    
    * initialized NavBar.js
    
    * forgot to add styles.scss and npm dependecies
    
    * finilized local storage, NavBar.js, and Players.js

---
+++ client/src/Components/Players.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+export class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        this.setState({
+          players: res.data
+
+        })
+      })
+  }
+  render() {
+    return (
+    )
+  }
+} 
